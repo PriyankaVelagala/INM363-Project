34a52eb4033b1194b9acb17473768bb9058d7dba
PriyankaVelagala <priyanka@velagala.org>
2022-08-14 20:21:02 +0100

    copy def scenario for vizdoom/examples

--- pytorch_example.py
+++ pytorch_example.py
@@ -1,126 +1,332 @@
+#cloned from examples/python/learning_pytorch.py
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-#####################################################################
-# This script demonstrates how to use the environment with PyOblige.
-# https://github.com/mwydmuch/PyOblige
-#####################################################################
+# E. Culurciello, L. Mueller, Z. Boztoprak
+# December 2020
 
-from argparse import ArgumentParser
-import os
-from time import sleep
-
-import oblige
 import vizdoom as vzd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import random
+import itertools as it
+import skimage.transform
 
-DEFAULT_CONFIG = "../../scenarios/oblige.cfg"
-DEFAULT_SEED = 666
-DEFAULT_OUTPUT_FILE = "test.wad"
+import os
+from time import sleep, time
+from collections import deque
+from tqdm import trange
 
-if __name__ == "__main__":
-    parser = ArgumentParser("An example showing how to generate maps with PyOblige.")
-    parser.add_argument(dest="config",
-                        default=DEFAULT_CONFIG,
-                        nargs="?",
-                        help="Path to the configuration file of the scenario."
-                             " Please see "
-                             "../../scenarios/*cfg for more scenarios.")
-    parser.add_argument("-s", "--seed",
-                        default=DEFAULT_SEED,
-                        type=int,
-                        help="Number of iterations(actions) to run")
-    parser.add_argument("-v", "--verbose",
-                        action="store_true",
-                        help="Use verbose mode during map generation.")
-    parser.add_argument("-o", "--output_file",
-                        default=DEFAULT_OUTPUT_FILE,
-                        help="Where the wad file will be created.")
-    parser.add_argument("-x", "--exit",
-                        action="store_true",
-                        help="Do not test the wad, just leave after generation.")
+# Q-learning settings
+learning_rate = 0.00025
+discount_factor = 0.99
+train_epochs = 5
+learning_steps_per_epoch = 2000
+replay_memory_size = 10000
 
-    args = parser.parse_args()
+# NN learning settings
+batch_size = 64
 
+# Training regime
+test_episodes_per_epoch = 100
+
+# Other parameters
+frame_repeat = 12
+resolution = (30, 45)
+episodes_to_watch = 10
+
+model_savefile = "./model-doom.pth"
+save_model = True
+load_model = False
+skip_learning = False
+
+# Configuration file path
+config_file_path = os.path.join(vzd.scenarios_path, "simpler_basic.cfg")
+# config_file_path = os.path.join(vzd.scenarios_path, "rocket_basic.cfg")
+# config_file_path = os.path.join(vzd.scenarios_path, "basic.cfg")
+
+# Uses GPU if available
+if torch.cuda.is_available():
+    DEVICE = torch.device('cuda')
+    torch.backends.cudnn.benchmark = True
+else:
+    DEVICE = torch.device('cpu')
+
+
+def preprocess(img):
+    """Down samples image to resolution"""
+    img = skimage.transform.resize(img, resolution)
+    img = img.astype(np.float32)
+    img = np.expand_dims(img, axis=0)
+    return img
+
+
+def create_simple_game():
+    print("Initializing doom...")
     game = vzd.DoomGame()
-    # Use your config
-    game.load_config(args.config)
-    game.set_doom_map("map01")
-    game.set_doom_skill(3)
-
-    # Create Doom Level Generator instance and set optional seed.
-    generator = oblige.DoomLevelGenerator()
-    generator.set_seed(args.seed)
-
-    # Set generator configs, specified keys will be overwritten.
-    generator.set_config({
-        "size": "micro",
-        "health": "more",
-        "weapons": "sooner"})
-
-    # There are few predefined sets of settings already defined in Oblige package, like test_wad and childs_play_wad
-    generator.set_config(oblige.childs_play_wad)
-
-    # Tell generator to generate few maps (options for "length": "single", "few", "episode", "game").
-    generator.set_config({"length": "few"})
-
-    # Generate method will return number of maps inside wad file.
-    wad_path = args.output_file
-    print("Generating {} ...".format(wad_path))
-    num_maps = generator.generate(wad_path, verbose=args.verbose)
-    print("Generated {} maps.".format(num_maps))
-
-    if args.exit:
-        exit(0)
-
-    # Set Scenario to the new generated WAD
-    game.set_doom_scenario_path(args.output_file)
-
-    # Sets up game for spectator (you)
-    game.add_game_args("+freelook 1")
+    game.load_config(config_file_path)
+    game.set_window_visible(False)
+    game.set_mode(vzd.Mode.PLAYER)
+    game.set_screen_format(vzd.ScreenFormat.GRAY8)
     game.set_screen_resolution(vzd.ScreenResolution.RES_640X480)
-    game.set_window_visible(True)
-    game.set_mode(vzd.Mode.SPECTATOR)
-
     game.init()
+    print("Doom initialized.")
 
-    # Play as many episodes as maps in the new generated WAD file.
-    episodes = num_maps
+    return game
 
-    # Play until the game (episode) is over.
-    for i in range(1, episodes + 1):
 
-        # Update map name
-        print("Map {}/{}".format(i, episodes))
-        map = "map{:02}".format(i)
-        game.set_doom_map(map)
+def test(game, agent):
+    """Runs a test_episodes_per_epoch episodes and prints the result"""
+    print("\nTesting...")
+    test_scores = []
+    for test_episode in trange(test_episodes_per_epoch, leave=False):
         game.new_episode()
-
-        time = 0
         while not game.is_episode_finished():
-            state = game.get_state()
-            time = game.get_episode_time()
+            state = preprocess(game.get_state().screen_buffer)
+            best_action_index = agent.get_action(state)
 
-            game.advance_action()
-            last_action = game.get_last_action()
-            reward = game.get_last_reward()
+            game.make_action(actions[best_action_index], frame_repeat)
+        r = game.get_total_reward()
+        test_scores.append(r)
 
-            print("State #" + str(state.number))
-            print("Game variables: ", state.game_variables)
-            print("Action:", last_action)
-            print("Reward:", reward)
-            print("=====================")
+    test_scores = np.array(test_scores)
+    print("Results: mean: %.1f +/- %.1f," % (
+        test_scores.mean(), test_scores.std()), "min: %.1f" % test_scores.min(),
+          "max: %.1f" % test_scores.max())
 
-        print("Episode finished!")
-        print("Total reward:", game.get_total_reward())
-        print("Kills:", game.get_game_variable(vzd.GameVariable.KILLCOUNT))
-        print("Items:", game.get_game_variable(vzd.GameVariable.ITEMCOUNT))
-        print("Secrets:", game.get_game_variable(vzd.GameVariable.SECRETCOUNT))
-        print("Time:", time / 35, "s")
-        print("************************")
-        sleep(2.0)
+
+def run(game, agent, actions, num_epochs, frame_repeat, steps_per_epoch=2000):
+    """
+    Run num epochs of training episodes.
+    Skip frame_repeat number of frames after each action.
+    """
+
+    start_time = time()
+
+    for epoch in range(num_epochs):
+        game.new_episode()
+        train_scores = []
+        global_step = 0
+        print("\nEpoch #" + str(epoch + 1))
+
+        for _ in trange(steps_per_epoch, leave=False):
+            state = preprocess(game.get_state().screen_buffer)
+            action = agent.get_action(state)
+            reward = game.make_action(actions[action], frame_repeat)
+            done = game.is_episode_finished()
+
+            if not done:
+                next_state = preprocess(game.get_state().screen_buffer)
+            else:
+                next_state = np.zeros((1, 30, 45)).astype(np.float32)
+
+            agent.append_memory(state, action, reward, next_state, done)
+
+            if global_step > agent.batch_size:
+                agent.train()
+
+            if done:
+                train_scores.append(game.get_total_reward())
+                game.new_episode()
+
+            global_step += 1
+
+        agent.update_target_net()
+        train_scores = np.array(train_scores)
+
+        print("Results: mean: %.1f +/- %.1f," % (train_scores.mean(), train_scores.std()),
+              "min: %.1f," % train_scores.min(), "max: %.1f," % train_scores.max())
+
+        test(game, agent)
+        if save_model:
+            print("Saving the network weights to:", model_savefile)
+            torch.save(agent.q_net, model_savefile)
+        print("Total elapsed time: %.2f minutes" % ((time() - start_time) / 60.0))
 
     game.close()
+    return agent, game
 
-    # Remove output of generator
-    os.remove(wad_path)
-    os.remove(wad_path.replace("wad", "old"))
-    os.remove(wad_path.replace("wad", "txt"))
+
+class DuelQNet(nn.Module):
+    """
+    This is Duel DQN architecture.
+    see https://arxiv.org/abs/1511.06581 for more information.
+    """
+
+    def __init__(self, available_actions_count):
+        super(DuelQNet, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=3, stride=2, bias=False),
+            nn.BatchNorm2d(8),
+            nn.ReLU()
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(8, 8, kernel_size=3, stride=2, bias=False),
+            nn.BatchNorm2d(8),
+            nn.ReLU()
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(8, 8, kernel_size=3, stride=1, bias=False),
+            nn.BatchNorm2d(8),
+            nn.ReLU()
+        )
+
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(8, 16, kernel_size=3, stride=1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU()
+        )
+
+        self.state_fc = nn.Sequential(
+            nn.Linear(96, 64),
+            nn.ReLU(),
+            nn.Linear(64, 1)
+        )
+
+        self.advantage_fc = nn.Sequential(
+            nn.Linear(96, 64),
+            nn.ReLU(),
+            nn.Linear(64, available_actions_count)
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = x.view(-1, 192)
+        x1 = x[:, :96]  # input for the net to calculate the state value
+        x2 = x[:, 96:]  # relative advantage of actions in the state
+        state_value = self.state_fc(x1).reshape(-1, 1)
+        advantage_values = self.advantage_fc(x2)
+        x = state_value + (advantage_values - advantage_values.mean(dim=1).reshape(-1, 1))
+
+        return x
+
+class DQNAgent:
+    def __init__(self, action_size, memory_size, batch_size, discount_factor,
+                 lr, load_model, epsilon=1, epsilon_decay=0.9996, epsilon_min=0.1):
+        self.action_size = action_size
+        self.epsilon = epsilon
+        self.epsilon_decay = epsilon_decay
+        self.epsilon_min = epsilon_min
+        self.batch_size = batch_size
+        self.discount = discount_factor
+        self.lr = lr
+        self.memory = deque(maxlen=memory_size)
+        self.criterion = nn.MSELoss()
+
+        if load_model:
+            print("Loading model from: ", model_savefile)
+            self.q_net = torch.load(model_savefile)
+            self.target_net = torch.load(model_savefile)
+            self.epsilon = self.epsilon_min
+
+        else:
+            print("Initializing new model")
+            self.q_net = DuelQNet(action_size).to(DEVICE)
+            self.target_net = DuelQNet(action_size).to(DEVICE)
+
+        self.opt = optim.SGD(self.q_net.parameters(), lr=self.lr)
+
+    def get_action(self, state):
+        if np.random.uniform() < self.epsilon:
+            return random.choice(range(self.action_size))
+        else:
+            state = np.expand_dims(state, axis=0)
+            state = torch.from_numpy(state).float().to(DEVICE)
+            action = torch.argmax(self.q_net(state)).item()
+            return action
+
+    def update_target_net(self):
+        self.target_net.load_state_dict(self.q_net.state_dict())
+
+    def append_memory(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def train(self):
+        batch = random.sample(self.memory, self.batch_size)
+        batch = np.array(batch, dtype=object)
+
+        states = np.stack(batch[:, 0]).astype(float)
+        actions = batch[:, 1].astype(int)
+        rewards = batch[:, 2].astype(float)
+        next_states = np.stack(batch[:, 3]).astype(float)
+        dones = batch[:, 4].astype(bool)
+        not_dones = ~dones
+
+        row_idx = np.arange(self.batch_size)  # used for indexing the batch
+
+        # value of the next states with double q learning
+        # see https://arxiv.org/abs/1509.06461 for more information on double q learning
+        with torch.no_grad():
+            next_states = torch.from_numpy(next_states).float().to(DEVICE)
+            idx = row_idx, np.argmax(self.q_net(next_states).cpu().data.numpy(), 1)
+            next_state_values = self.target_net(next_states).cpu().data.numpy()[idx]
+            next_state_values = next_state_values[not_dones]
+
+        # this defines y = r + discount * max_a q(s', a)
+        q_targets = rewards.copy()
+        q_targets[not_dones] += self.discount * next_state_values
+        q_targets = torch.from_numpy(q_targets).float().to(DEVICE)
+
+        # this selects only the q values of the actions taken
+        idx = row_idx, actions
+        states = torch.from_numpy(states).float().to(DEVICE)
+        action_values = self.q_net(states)[idx].float().to(DEVICE)
+
+        self.opt.zero_grad()
+        td_error = self.criterion(q_targets, action_values)
+        td_error.backward()
+        self.opt.step()
+
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+        else:
+            self.epsilon = self.epsilon_min
+
+
+if __name__ == '__main__':
+    # Initialize game and actions
+    game = create_simple_game()
+    n = game.get_available_buttons_size()
+    actions = [list(a) for a in it.product([0, 1], repeat=n)]
+
+    # Initialize our agent with the set parameters
+    agent = DQNAgent(len(actions), lr=learning_rate, batch_size=batch_size,
+                     memory_size=replay_memory_size, discount_factor=discount_factor,
+                     load_model=load_model)
+
+    # Run the training for the set number of epochs
+    if not skip_learning:
+        agent, game = run(game, agent, actions, num_epochs=train_epochs, frame_repeat=frame_repeat,
+                          steps_per_epoch=learning_steps_per_epoch)
+
+        print("======================================")
+        print("Training finished. It's time to watch!")
+
+    # Reinitialize the game with window visible
+    game.close()
+    game.set_window_visible(True)
+    game.set_mode(vzd.Mode.ASYNC_PLAYER)
+    game.init()
+
+    for _ in range(episodes_to_watch):
+        game.new_episode()
+        while not game.is_episode_finished():
+            state = preprocess(game.get_state().screen_buffer)
+            best_action_index = agent.get_action(state)
+
+            # Instead of make_action(a, frame_repeat) in order to make the animation smooth
+            game.set_action(actions[best_action_index])
+            for _ in range(frame_repeat):
+                game.advance_action()
+
+        # Sleep between episodes
+        sleep(1.0)
+        score = game.get_total_reward()
+        print("Total score: ", score)
